192ab6afc4bbc9b2550b830bcab0bf77c78893b9
Zubair Muwwakil <zmuwwakil@gmail.com>
2025-12-10 10:07:03 -0500

    arithmetic operations in java, loops., augmented assignment operations, incremenet and decrement operations

---
+++ arithmetic.java
@@ -0,0 +1,42 @@
+public class FirstProject {
+
+    public static void main (String[] args){
+
+        //arithmetic operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        // z = x % y when x divides y what is the remainder 
+
+
+        //Augumented Assignment Operators
+
+        //x=x+y is same as x+=y
+        // x=x-y same as x-=y
+        // ....         x/=y
+
+        // for int division its whole number used double to get decimals.
+
+
+        //Increment and Decrement Operators (good for loops )
+
+        // x++ increments x by 1 x=x+1
+        // x-- .....
+
+
+
+        // Order of operations [pemdas] is used 
+
+
+
+
+
+        
+          
+
+    }
+
+}
