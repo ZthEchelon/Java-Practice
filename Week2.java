e15080aeb11982c3adf1a992b30696b9d9dc8abb
Zubair Muwwakil <zmuwwakil@gmail.com>
2025-12-10 08:37:59 -0500

    initial commit

--- Week2.java
+++ Week2.java
@@ -4,7 +4,7 @@ public class Week2 {
 
         byte x = 123; //can only take values from -128 to 127
         float y=3.14f;//needs f at the end
-        double t=2.89;
+        double    git commit -m "Your descriptive commit message" t=2.89;
 
         System.out.println("Here are some nums "+ x+y+t);
 
